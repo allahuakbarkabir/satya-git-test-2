8eec09f10fb3a6a4d5b87d32916c9012c7d2cab2
allahuakbarkabir <satyaandhika000@gmail.com>
2024-10-03 11:17:02 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto autem consequuntur atque earum amet iure, distinctio expedita optio, nostrum eos ex quod? Debitis id natus iste ullam exercitationem velit dolores.</p>
+    <ul>
+        <li>Asep Gagah</li>
+    </ul>
 </body>
 </html>
